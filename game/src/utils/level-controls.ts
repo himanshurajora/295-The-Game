1fc11f5ee5d4b3485a2e313350feb57cb88a8648
himanshurajora <iamahalofan@gmail.com>
2022-06-13 19:50:16 +0530

    level enemy spawn by kills

--- game/src/utils/level-controls.ts
+++ game/src/utils/level-controls.ts
@@ -1,4 +1,11 @@
 import {
+  ENEMY_RESPAWN_TIME_DELAY,
+  ENEMY_RESPAWN_TIME_DELAY_LEVEL2,
+  ENEMY_RESPAWN_TIME_DELAY_LEVEL3,
+  ENEMY_RESPAWN_TIME_DELAY_LEVEL4,
+  ENEMY_RESPAWN_TIME_DELAY_LEVEL5,
+  PLAYER_FIRE_RATE,
+  PLAYER_FIRE_RATE_AFTER,
   PLAYER_FIRE_RATE_LEVEL2,
   PLAYER_FIRE_RATE_LEVEL3,
   PLAYER_FIRE_RATE_LEVEL4,
@@ -6,26 +13,55 @@ import {
 } from '../constants/GameConstants';
 import { Player } from '../objects/Player';
 
+/**
+ * Controls the rate of fire of the player.
+ * @param {Player} player the main player
+ */
 export function setPlayerControlByLevel(player: Player) {
-  const level = player.killCount / 10;
+  const level = player.killCount / 50;
 
   switch (Math.floor(level)) {
+    case 0:
+      player.fireRate = PLAYER_FIRE_RATE;
+      break;
     case 1:
-      player.fireRate = PLAYER_FIRE_RATE_LEVEL2;
+      player.fireRate = PLAYER_FIRE_RATE;
       break;
     case 2:
-      player.fireRate = PLAYER_FIRE_RATE_LEVEL3;
+      player.fireRate = PLAYER_FIRE_RATE_LEVEL2;
       break;
     case 3:
-      player.fireRate = PLAYER_FIRE_RATE_LEVEL4;
+      player.fireRate = PLAYER_FIRE_RATE_LEVEL3;
       break;
     case 4:
-      player.fireRate = PLAYER_FIRE_RATE_LEVEL5;
+      player.fireRate = PLAYER_FIRE_RATE_LEVEL4;
       break;
     case 5:
-      player.fireRate = PLAYER_FIRE_RATE_LEVEL5 * 2;
+      player.fireRate = PLAYER_FIRE_RATE_LEVEL5;
       break;
     default:
+      player.fireRate = PLAYER_FIRE_RATE_AFTER;
       break;
+  }
+}
+
+export function setEnemyControlByLevel(kills: number) {
+  const level = kills / 50;
+
+  switch (Math.floor(level)) {
+    case 0:
+      return ENEMY_RESPAWN_TIME_DELAY;
+    case 1:
+      return ENEMY_RESPAWN_TIME_DELAY;
+    case 2:
+      return ENEMY_RESPAWN_TIME_DELAY_LEVEL2;
+    case 3:
+      return ENEMY_RESPAWN_TIME_DELAY_LEVEL3;
+    case 4:
+      return ENEMY_RESPAWN_TIME_DELAY_LEVEL4;
+    case 5:
+      return ENEMY_RESPAWN_TIME_DELAY_LEVEL5;
+    default:
+      return 200;
   }
 }
